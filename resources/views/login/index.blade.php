8916f4a66e7c6acc6b4146ef5c868d72b37eb68e
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2024-05-17 23:55:31 -0600

    estilos de login y register
    se modifico ya funciona los estilos completos de login y register

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,25 +5,41 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="{{ asset('./css/login.css') }}" rel="stylesheet">
 </head>
 
 <body>
 
-    <div class="container mt-5">
-        <form action="{{ route('inicioSesion') }}" method="post">
-            {{ csrf_field() }}
-            <input type="text" class="form-control" placeholder="E-mail" name="correo" id="correo">
-            <input type="text" class="form-control" placeholder="Contraseña" name="clave" id="clave">
-            {{-- <input type="text" class="form-control" placeholder="clave" name="clave" id="clave"> --}}
-            <input type="submit" class="form-control bg-primary text-light " value="Iniciar Sesion">
-        </form>
+    <div class="container">
+        <div class="user_card">
+            <div class="brand_logo_container">
+                <img src="{{ asset('img/logo.png') }}" class="brand_logo" alt="Logo">
+            </div>
+            <div class="form_container">
+                <form action="{{ route('inicioSesion') }}" method="post">
+                    {{ csrf_field() }}
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control input_user" placeholder="E-mail" name="correo" id="correo" required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control input_pass" placeholder="Contraseña" name="clave" id="clave" required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="submit" class="form-control bg-primary text-light login_btn" value="Iniciar Sesión">
+                    </div>
+                </form>
+                <div class="separator">
+                    <div class="line"></div>
+                    <div class="or">O</div>
+                    <div class="line"></div>
+                </div>
+                <div class="register_container">
+                    <p class="register_text">¿No tienes una cuenta? <a href="{{ route('registro') }}" class="register_btn">Regístrate</a></p>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,11 +1,11 @@
-@csrf
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Registro</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="{{ asset('./css/registro.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
@@ -21,43 +21,57 @@
             </div>
         @endif
 
-        <form action="{{ route('registro') }}" method="post">
-            @csrf
-            <div class="mb-3">
-                <label for="nombre_cliente" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="nombre_cliente" name="nombre_cliente" placeholder="Nombre" required>
+        <div class="user_card">
+            <div class="brand_logo_container">
+                <img src="{{ asset('img/logo_delgado.png') }}" class="brand_logo" alt="Logo">
+                <p class="subparrafo">Regístrate para reservar una cita o ver los productos.</p>
             </div>
-            <div class="mb-3">
-                <label for="apellido_cliente" class="form-label">Apellido</label>
-                <input type="text" class="form-control" id="apellido_cliente" name="apellido_cliente" placeholder="Apellido" required>
+            <div class="form_container">
+                <form action="{{ route('registro') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        
+                        <input type="text" class="form-control" id="nombre_cliente" name="nombre_cliente" placeholder="Nombre" required>
+                    </div>
+                    <div class="mb-3">
+                        
+                        <input type="text" class="form-control" id="apellido_cliente" name="apellido_cliente" placeholder="Apellido" required>
+                    </div>
+                    <div class="mb-3">
+                        
+                        <input type="text" class="form-control" id="telefono_cliente" name="telefono_cliente" placeholder="Teléfono" required>
+                    </div>
+                    <div class="mb-3">
+                    
+                        <input type="text" class="form-control" id="direccion_cliente" name="direccion_cliente" placeholder="Dirección" required>
+                    </div>
+                    <div class="mb-3">
+                        
+                        <input type="text" class="form-control" id="dui_cliente" name="dui_cliente" placeholder="DUI" required>
+                    </div>
+                    <div class="mb-3">
+                        
+                        <input type="email" class="form-control" id="email_cliente" name="email_cliente" placeholder="Correo Electrónico" required>
+                    </div>
+                    <div class="mb-3">
+                        
+                        <input type="password" class="form-control" id="clave_cliente" name="clave_cliente" placeholder="Contraseña" required>
+                    </div>
+        
+                    <button type="submit" class="btn btn-primary">Registrarse</button>
+                </form>
+
+                <div class="separator">
+                    <div class="line"></div>
+                    <div class="or">O</div>
+                    <div class="line"></div>
+                </div>
+
+                <div class="login_link_container">
+                    <p class="login_link_text">¿Tienes una cuenta? <a href="{{ route('login') }}" class="login_link">Inicia sesión</a></p>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="telefono_cliente" class="form-label">Teléfono</label>
-                <input type="text" class="form-control" id="telefono_cliente" name="telefono_cliente" placeholder="Teléfono" required>
-            </div>
-            <div class="mb-3">
-                <label for="direccion_cliente" class="form-label">Dirección</label>
-                <input type="text" class="form-control" id="direccion_cliente" name="direccion_cliente" placeholder="Dirección" required>
-            </div>
-            <div class="mb-3">
-                <label for="dui_cliente" class="form-label">DUI</label>
-                <input type="text" class="form-control" id="dui_cliente" name="dui_cliente" placeholder="DUI" required>
-            </div>
-            <div class="mb-3">
-                <label for="email_cliente" class="form-label">Correo Electrónico</label>
-                <input type="email" class="form-control" id="email_cliente" name="email_cliente" placeholder="Correo Electrónico" required>
-            </div>
-            <div class="mb-3">
-                <label for="clave_cliente" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="clave_cliente" name="clave_cliente" placeholder="Contraseña" required>
-            </div>
-            <div class="mb-3">
-                <label for="img_user" class="form-label">Imagen de Perfil (URL)</label>
-                <input type="text" class="form-control" id="img_user" name="img_user" placeholder="Imagen de Perfil (URL)">
-            </div>
-            <button type="submit" class="btn btn-primary">Registrarse</button>
-        </form>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
